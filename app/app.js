751a5604c2ca02415d6e6728254c64e2b00167c0
Autobalanced <scwables@gmail.com>
2014-11-05 15:45:17 +1100

    added Typeform integrated survey to survey list.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 // Declare app level module which depends on filters, and services
 
-(function() {
+(function()
+{
     'use strict';
 
     angular
@@ -10,56 +11,73 @@
         ])
         .config(config);
 
-    function config($routeProvider) {
+    function config($routeProvider)
+    {
         $routeProvider
-            .when('/', {
+            .when('/',
+            {
                 templateUrl: 'main/landing_page.html',
                 controller: 'MainController',
                 controllerAs: 'main'
             });
         $routeProvider
-            .when('/login', {
+            .when('/login',
+            {
                 templateUrl: 'accounts/login.html',
                 controller: 'AuthController',
                 controllerAs: 'auth'
             });
         $routeProvider
-            .when('/register', {
+            .when('/register',
+            {
                 templateUrl: 'accounts/register.html',
                 controller: 'AuthController',
                 controllerAs: 'auth'
             });
         $routeProvider
-        	.when('/survey_list', {
-        		templateUrl: 'surveylist/survey_list.html',
-        		controller: 'SurveyListController',
-        		controllerAs: 'surveyList'
-        	});
+            .when('/survey_list',
+            {
+                templateUrl: 'surveylist/survey_list.html',
+                controller: 'SurveyListController',
+                controllerAs: 'surveyList'
+            });
         $routeProvider
-            .when('/survey', {
+            .when('/survey',
+            {
                 templateUrl: 'survey/survey.html',
                 controller: 'SurveyController',
                 controllerAs: 'sv'
             });
         $routeProvider
-            .when('/create', {
+            .when('/create',
+            {
                 templateUrl: 'cms/create.html',
                 controller: 'CmsController',
                 controllerAs: 'cms'
             });
         $routeProvider
-            .when('/client_login', {
+            .when('/client_login',
+            {
                 templateUrl: 'accounts/clients/client_login.html',
                 controller: 'ClientAuthController',
                 controllerAs: 'clientAuth'
             });
         $routeProvider
-            .when('/client_register', {
+            .when('/client_register',
+            {
                 templateUrl: 'accounts/clients/client_register.html',
                 controller: 'ClientAuthController',
                 controllerAs: 'clientAuth'
             });
-        $routeProvider.otherwise({
+        $routeProvider
+            .when('/typeformsurvey',
+            {
+                templateUrl: 'typeform/typeformsurvey.html',
+                controller: 'TypeformController',
+                controllerAs: 'type'
+            });
+        $routeProvider.otherwise(
+        {
             redirectTo: '/'
         });
     }
---
+++ app/typeform/typeform.controller.js
@@ -0,0 +1,11 @@
+(function(){
+	'use strict';
+
+	angular
+		.module('crowdApp')
+		.controller('TypeformController', TypeformController);
+
+	function TypeformController(){
+		
+	}
+})();
