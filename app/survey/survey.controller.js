f6a9c8e55b8b04a1e7bdf875380eb9704d0be003
Autobalanced <scwables@gmail.com>
2014-11-10 17:05:16 +1100

    added pagination code to survey.html

--- app/survey/survey.controller.js
+++ app/survey/survey.controller.js
@@ -14,6 +14,37 @@
 
 		vm.survey = SurveyService.getSurvey();
 		console.log(vm.survey);
-	
+
+		// Set current number of questions to 0
+		vm.currentQuestion = 0;
+		// Get max questions of survey
+		vm.maxQuestions = (vm.survey.$child('questions').$getIndex().length) - 1;	
+
+		vm.isSet = function(checkQuestion)
+		{
+			return vm.currentQuestion === checkQuestion;
+		};
+
+		vm.setQuestion = function(activeQuestion)
+		{
+			vm.currentQuestion = activeQuestion;
+		};
+
+		vm.prevQuestion = function()
+		{
+			if (vm.currentQuestion > 0)
+			{
+				vm.currentQuestion--;
+			}
+		};
+
+		vm.nextQuestion = function()
+		{
+			if (vm.currentQuestion < vm.maxQuestions)
+			{
+				vm.currentQuestion++;
+			}
+		};
+
 	}
 })();
